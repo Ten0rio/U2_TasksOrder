813208667913ead0bb1da44a91526e19e510092b
Fabian <fabian.bender@smail.inf.h-brs.de>
2020-05-04 11:14:10 +0200

    Bla

---
+++ src/Sequenz.java
@@ -0,0 +1,14 @@
+public class Sequenz implements SequenzIF {
+
+   String [][] sequenz;
+
+   public Sequenz (String [][] other){
+       this.sequenz = other;
+   }
+
+
+    @Override
+    public boolean isWellSorted(String[] other) {
+        return false;
+    }
+}
