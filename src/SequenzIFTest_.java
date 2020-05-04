de28c68749855f5062e3c9416b26675e733a11d4
lwerte2s <luwer3@googlemail.com>
2020-05-04 14:36:35 +0200

    test bearbeitet

--- src/SequenzIFTest_.java
+++ src/SequenzIFTest_.java
@@ -7,37 +7,40 @@ import org.junit.jupiter.api.Test;
 class SequenzIFTest_ {
 	
 	String[][] s1 = {{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
-	String[][] s2 = {{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
+	String[][] s3 = {{ "A" , "C" }, { "B", "C" }, { "D", "C" },{"C","E"},{"E","F"}};
+	
 	Sequenz t1;
 	Sequenz t2;
+	Sequenz t3;
 	
 	@BeforeEach
 	void setUp() {
 		t1 = new Sequenz(s1);
-		t2 = new Sequenz(s2);
+		t2 = new Sequenz(s1);
+		t3 = new Sequenz(s3);
 	}
 	
 
 	@Test
-	@DisplayName("Die Reihenfolgen muss ausfÃ¼hrbar sein und den AbhÃ¤ngigkeiten entsprechen")
+	@DisplayName("Bei richtig sortierter Sequenz gibt isWellSorted 'true' aus")
 	void test() {
 		String[] seq =  {"A" , "B", "C", "D"};
-		String[] seq2 = { "D" , "C", "B", "A" };
+		String[] seq2 = { "D" , "C", "B" , "A"};
+		String[] seq3 = { "A" , "B", "D", "C", "E", "F"};
 
-		//schlaegt feht
 		assertEquals(true, t1.isWellSorted(seq));
 		assertEquals(false, t1.isWellSorted(seq2));
+		
+		assertEquals(false, t3.isWellSorted(seq2));
+		assertEquals(true, t3.isWellSorted(seq3));
 
-		// wird gruen
-		//assertEquals(false, t1.isWellSorted(seq2));
-		//assertEquals(true, t1.isWellSorted(seq));
 
 		
 		
 	}
 
 	@Test
-	@DisplayName("Die Objekte sollen gleich sein wenn das gespeicherte aktualisiert wird")
+	@DisplayName("setSequence soll die Sequenz zu der übergebenen Sequenz verändern")
     public void setSequence() {
         String [][] test = {{"A","B"},{"A","B"}};
         t1.setSequence(test);
