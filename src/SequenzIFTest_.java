a1a92931171fca750dcae54d2e494938f70ada4a
Ten0rio <63906477+Ten0rio@users.noreply.github.com>
2020-05-08 20:08:11 +0200

    hope old version

---
+++ src/SequenzIFTest_.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SequenzIFTest_ {
+	
+	String[][] s1 = {{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
+	String[][] s2 = {{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
+	Sequenz t1;
+	Sequenz t2;
+	
+	@BeforeEach
+	void setUp() {
+		t1 = new Sequenz(s1);
+		t2 = new Sequenz(s2);
+	}
+	
+
+	@Test
+	@DisplayName("Die Reihenfolgen muss ausführbar sein und den Abhängigkeiten entsprechen")
+	void test() {
+		String[] seq =  {"A" , "B", "C", "D"};
+		String[] seq2 = { "D" , "C", "B", "A" };
+
+		//schlaegt feht
+		assertEquals(true, t1.isWellSorted(seq));
+		assertEquals(false, t1.isWellSorted(seq2));
+
+		// wird gruen
+		//assertEquals(false, t1.isWellSorted(seq2));
+		//assertEquals(true, t1.isWellSorted(seq));
+
+		
+		
+	}
+
+	@Test
+	@DisplayName("Die Objekte sollen gleich sein wenn das gespeicherte aktualisiert wird")
+    public void setSequence() {
+        String [][] test = {{"A","B"},{"A","B"}};
+        t1.setSequence(test);
+        assertEquals(test,t1.getSequence());
+    }
+	
+}
